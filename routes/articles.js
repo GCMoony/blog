137e02a19760a16345ae4b8af339024452206a89
GCMoony <47188474+GCMoony@users.noreply.github.com>
2023-02-18 17:15:01 -0700

    todo: connect to db

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,39 @@
 const express = require('express')
+const article = require('./../models/article')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send("In articles")
+const Article = require('./../models/article')
+
+// router.get('/', (req, res) => {
+//     res.send("In articles")
+// })
+
+// This gets you to a new html page called "new"
+router.get('/new', (req, res) => {
+    res.render("articles/new")
+})
+
+// This gets you the article with the specified ID
+router.get('/:id', (req, res) => {
+
+})
+
+// This posts a form item to the index page
+router.post('/', async (req, res) => {
+    const article = new Article({
+        // Get the element values via element ID
+        title: req.body.title,
+        description: req.body.description,
+        markdown: req.body.markdown,
+    })
+
+    try {
+        article = await article.save()
+        res.redirect(`/articles/${article.id}`)
+    } catch (e) {
+        res.render('articles/new', { article: article })
+    }
+    
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,33 +1,59 @@
+/*
+    To run the server:
+
+        npm run [scriptName]
+
+        i.e. this project
+
+        npm run devStart
+*/
+
 const express = require('express')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/blog');
+mongoose.set('strictQuery', false);
+
 // Sets up the view
 app.set('view engine', 'ejs')
 
-// Uses the article router
-app.use('/articles', articleRouter)
+
+
+// Use the public folder for different assets, like pics or stylesheets
+app.use('/public', express.static('public'));
+
+// To get the form data
+app.use(express.urlencoded({ extended: false }))
+
+
 
 // To render the index view
+// This will show in the index.ejs file
 app.get('/', (req, res) => {
     const articles = [
         {
         title: "Test Article",
-        createdAt: Date.now(),
+        createdAt: new Date(),
         description: 'Test Description'
         },
         {
             title: "Test Article Numba 2",
-            createdAt: Date.now(),
+            createdAt: new Date(),
             description: 'Test Article 2 Description'
         },
         {
             title: "Test Article Numba 3",
-            createdAt: Date.now(),
+            createdAt: new Date(),
             description: 'Test Article 3 Description'
         }
     ]
-    res.render('index', { articles: articles })
+    res.render('articles/index', { articles: articles })
 })
+
+// Uses the article router
+app.use('/articles', articleRouter)
 
 app.listen(5000)
